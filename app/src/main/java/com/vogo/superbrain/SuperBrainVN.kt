ad29b7e12b885696d7c1f925cbcf63708bfed27a
lorence <lorence@enclave.vn>
2020-04-29 11:12:00 +0700

    [] (Lorence) Implement Koin for learning

--- app/src/main/java/com/vogo/superbrain/SuperBrainVN.kt
+++ app/src/main/java/com/vogo/superbrain/SuperBrainVN.kt
@@ -1,11 +1,21 @@
 package com.vogo.superbrain
 
 import android.app.Application
+import com.vogo.superbrain.frameworks.di.module.appModule
+import com.vogo.superbrain.frameworks.di.module.engineModule
+import com.vogo.superbrain.frameworks.di.module.networkModule
+import com.vogo.superbrain.frameworks.di.module.viewModelModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class SuperBrainVN : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin{
+            androidContext(this@SuperBrainVN)
+            listOf(modules(appModule, networkModule, viewModelModules, engineModule))
+        }
     }
 
 }
---
+++ app/src/main/java/com/vogo/superbrain/frameworks/di/module/modules.kt
@@ -0,0 +1,19 @@
+package com.vogo.superbrain.frameworks.di.module
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
+
+val networkModule = module {
+
+}
+
+val engineModule = module {
+
+}
+
+val viewModelModules = module {
+
+}
