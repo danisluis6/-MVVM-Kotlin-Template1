769ce9ed7c15501dc5c0d5b3bf9d00789d58753d
lorence <lorence@enclave.vn>
2020-04-29 10:03:27 +0700

    [] (Lorence) Optimize code

---
+++ app/src/main/java/com/vogo/superbrain/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.vogo.superbrain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
